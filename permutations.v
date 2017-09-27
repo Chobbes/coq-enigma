c62ab353cde48fcebc4dbca8c1eb729b4d73f766
Calvin Beck <hobbes@ualberta.ca>
2017-09-27 13:37:45 -0600

    Some more theorems about permutations and vectors.

--- permutations.v
+++ permutations.v
@@ -17,15 +17,15 @@ Qed.
 
 
 Inductive NoDupVec {A : Type} : forall {n : nat}, Vec A n -> Prop :=
-| NoDupVec_nil : NoDupVec (VectorDef.nil A)
-| NoDupVec_cons : forall x m (v : Vec A m), ~ VectorDef.In x v -> NoDupVec v -> NoDupVec (VectorDef.cons _ x m v)
+| NoDupVec_nil : NoDupVec (nil A)
+| NoDupVec_cons : forall x m (v : Vec A m), ~ In x v -> NoDupVec v -> NoDupVec (cons _ x m v)
 .
 
 
 Fixpoint in_vector {A : Type} {n : nat} (decA : forall x y : A, {x = y} + {x <> y}) (a : A) (v : Vec A n) : bool :=
   match v with
-  | VectorDef.nil _ => false
-  | VectorDef.cons _ x n t =>
+  | nil _ => false
+  | cons _ x n t =>
     if decA a x
     then true
     else in_vector decA a t
@@ -34,7 +34,7 @@ Fixpoint in_vector {A : Type} {n : nat} (decA : forall x y : A, {x = y} + {x <> 
 
 Theorem In_in_vector :
   forall {n : nat} {A : Type} (decA : forall x y : A, {x = y} + {x <> y}) (a : A) (v : Vec A n),
-    VectorDef.In a v -> in_vector decA a v = true.
+    In a v -> in_vector decA a v = true.
 Proof.
   intros n A decA a v H.
   induction H; simpl; destruct decA; try reflexivity; auto.
@@ -43,14 +43,14 @@ Qed.
 
 Theorem in_vector_In :
   forall {n : nat} {A : Type} (decA : forall x y : A, {x = y} + {x <> y}) (a : A) (v : Vec A n),
-    in_vector decA a v = true -> VectorDef.In a v.
+    in_vector decA a v = true -> In a v.
 Proof.
   intros n A decA a v H.
   induction v.
   - simpl in H. inversion H.
   - simpl in *. destruct (decA a h) eqn:HdecA.
-    + destruct HdecA. subst. apply VectorDef.In_cons_hd.
-    + apply VectorDef.In_cons_tl. auto.
+    + destruct HdecA. subst. apply In_cons_hd.
+    + apply In_cons_tl. auto.
 Qed.
 
 
@@ -60,12 +60,12 @@ Definition wf_permutation (perm : Vec Alpha 26) : Prop :=
 *)
 
 Inductive IsPermutation {A} : forall {n}, Vec A n -> Vec A n -> Prop :=
-| permNil : IsPermutation (VectorDef.nil _) (VectorDef.nil _)
+| permNil : IsPermutation (nil _) (nil _)
 | permSkip : forall {x m} (v1 : Vec A m) (v2 : Vec A m),
-    IsPermutation v1 v2 -> IsPermutation (VectorDef.cons _ x m v1) (VectorDef.cons _ x m v2)
+    IsPermutation v1 v2 -> IsPermutation (cons _ x m v1) (cons _ x m v2)
 | permSwap : forall {x y m} (v : Vec A m),
-    IsPermutation (VectorDef.cons _ x (S m) (VectorDef.cons _ y _ v))
-                  (VectorDef.cons _ y (S m) (VectorDef.cons _ x _ v))
+    IsPermutation (cons _ x (S m) (cons _ y _ v))
+                  (cons _ y (S m) (cons _ x _ v))
 | permTrans : forall {m} (v1 : Vec A m) (v2 : Vec A m) (v3 : Vec A m),
     IsPermutation v1 v2 -> IsPermutation v2 v3 -> IsPermutation v1 v3
 .
@@ -85,38 +85,149 @@ Qed.
 
 Theorem vec_0_is_nil :
   forall {A} (v : Vec A 0),
-    v = VectorDef.nil A.
+    v = nil A.
 Proof.
   intros A v. unfold Vec in *.
   refine (match v with
-          | VectorDef.nil _ => _
-          | VectorDef.cons _ x _ v => _
+          | nil _ => _
+          | cons _ x _ v => _
           end).
   reflexivity.
   apply idProp.
 Qed.
 
 
-(*
-Theorem in_perm :
-  forall {A n x} (v1 v2 : Vec A n),
-    VectorDef.In x v1 ->
-    IsPermutation v1 v2 ->
-    VectorDef.In x v2.
+Theorem Forall_hd :
+  forall {A n h} (f : A -> Prop) (v : Vec A n),
+    Forall f (cons _ h n v) ->
+    f h.
 Proof.
-  intros A n x v1 v2 Hin Hperm.
-  generalize dependent x.
-  generalize dependent v1.
-  generalize dependent v2.
-  induction n.
-  - intros v2 v1 Hperm x Hin.
-    pose proof vec_0_is_nil v1.
-    rewrite H in Hin. inversion Hin.
-  - intros v2 v1 Hperm x Hin.
-    
+  intros A n h f v H.
+  inversion H. assumption.
 Qed.
 
-  
+
+Theorem In_not_in_tl :
+  forall {A n x h} (v : Vec A n),
+    In x (cons _ h n v) ->
+    not (In x v) ->
+    x = h.
+Proof.
+  intros A n x h v Hin Hnin.
+  inversion Hin.
+  - reflexivity.
+  - apply inj_pair2 in H2. subst.
+    contradiction.
+Qed.
+
+
+Theorem Forall_In :
+  forall {A n} (f : A -> Prop) (v : Vec A n),
+    Forall f v ->
+    (forall x, In x v -> f x).
+Proof.  
+  intros A n f v Hall x Hin.
+  induction Hall.
+  - inversion Hin.
+  - inversion Hin.
+    + assumption.
+    + subst. apply inj_pair2 in H3. subst. auto.
+Qed.
+
+
+Theorem In_Forall :
+  forall {A n} (f : A -> Prop) (v : Vec A n),
+    (forall x, In x v -> f x) ->
+    Forall f v.
+Proof.
+  intros A n f v.
+  induction v.
+  - intros x. constructor.
+  - intros H. constructor.
+    + apply H. constructor.
+    + apply IHv. intros x Hin.
+      apply H. constructor.
+      assumption.
+Qed.
+
+
+Theorem Forall_tl :
+  forall {A n x} (f : A -> Prop) (v : Vec A n),
+    Forall f (cons A x n v) ->
+    Forall f v.
+Proof.
+  intros A n x f v H.
+  apply In_Forall.
+  pose proof Forall_In f (cons A x n v).
+  intros x0 H1.
+  apply H0.
+  assumption.
+  constructor.
+  assumption.
+Qed.
+
+
+Theorem forall_perm :
+  forall {A n} (f : A -> Prop) (v1 v2 : Vec A n),
+    Forall f v1 ->
+    IsPermutation v1 v2 ->
+    Forall f v2.
+Proof.
+  intros A n f v1 v2 Hall Hperm.
+  induction Hperm.
+  - constructor.
+  - constructor.
+    + inversion Hall; assumption.
+    + apply IHHperm. apply Forall_tl in Hall. assumption.
+  - constructor.
+    + apply Forall_tl in Hall. inversion Hall; assumption.
+    + constructor.
+      * inversion Hall; assumption.
+      * repeat apply Forall_tl in Hall. assumption.
+  - auto.
+Qed.    
+
+
+Theorem in_swap :
+  forall {A n x y1 y2} (v : Vec A n),
+    In x (cons A y1 (S n) (cons A y2 n v)) ->
+    In x (cons A y2 (S n) (cons A y1 n v)).
+Proof.
+  intros A n x y1 y2 v H.
+  inversion H.
+  - repeat constructor.
+  - subst. apply inj_pair2 in H3. subst.
+    inversion H2.
+    + constructor.
+    + subst. apply inj_pair2 in H4. subst.
+      repeat constructor.
+      assumption.
+Qed.
+
+
+Theorem in_perm :
+  forall {A n x} (v1 v2 : Vec A n),
+    In x v1 ->
+    IsPermutation v1 v2 ->
+    In x v2.
+Proof.
+  intros A n x v1 v2 Hin Hperm.
+  induction Hperm.
+  - assumption.
+  - inversion Hin.
+    + apply In_cons_hd.
+    + subst. apply inj_pair2 in H2. subst. constructor. auto.
+  - inversion Hin.
+    + repeat constructor.
+    + subst. apply inj_pair2 in H2. subst.
+      apply in_swap.
+      assumption.
+  - auto.
+Qed.
+
+
+
+(*
 Theorem no_dup_perm :
   forall {A n} (v1 v2 : Vec A n),
     NoDupVec v1 ->
@@ -134,7 +245,7 @@ Qed.
 
 Theorem wf_permutation_alphabet :
   forall perm,
-    VectorDef.Forall (fun a => VectorDef.In a perm) alphabet ->
+    Forall (fun a => In a perm) alphabet ->
     wf_permutation perm.
 Proof.
   intros perm H. unfold wf_permutation.
@@ -145,7 +256,7 @@ Qed.
 Theorem wf_permutation_alphabet :
   forall perm,
     wf_permutation perm ->
-    VectorDef.Forall (fun a => VectorDef.In a perm) alphabet.
+    Forall (fun a => In a perm) alphabet.
 Proof.
   unfold wf_permutation.
   intros perm H.
